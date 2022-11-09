fc3dde09449f948959ba4791427f3ba5f7f72bc4
Keks <keks@htmlacademy.ru>
2022-11-09 20:36:51 +0300

    :heavy_check_mark: Сборка #10

---
+++ 10/js/script.js
@@ -0,0 +1,11 @@
+let popup = document.querySelector('.popup');
+let closeButton = document.querySelector('.close-button');
+let buttonSearchHotels =document.querySelector('.button--search-hotels');
+
+closeButton.onclick = function () {
+    popup.classList.add('popup--hidden');
+}
+
+buttonSearchHotels.onclick = function () {
+    popup.classList.remove('popup--hidden');
+}
