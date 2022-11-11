357e73374986681f6cb28358211b33a1c16bc990
alex-kabanin <alexcobainius@gmail.com>
2022-11-11 13:37:30 +0400

    css fixed

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 let popup = document.querySelector('.popup');
 let closeButton = document.querySelector('.close-button');
-let buttonSearchHotels =document.querySelector('.button--search-hotels');
+let buttonSearchHotels = document.querySelector('.button--search-hotels');
 
 closeButton.onclick = function () {
     popup.classList.add('popup--hidden');
